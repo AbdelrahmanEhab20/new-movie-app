ada4dc0fd445a0b5dbcd84d89020dc3df0309e1a
AbdelrahmanEhab <abdoehab5008@gmail.com>
2026-01-17 20:55:21 +0200

    Initial start with the api and test the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Search from './components/Search'
 
+// API constants 
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TEMP_ACCESS_KEY_ID;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  //Function to fetch data from API
+  const fetchData = async (endpoint) => {
+    try {
+      const response = await fetch(`${API_BASE_URL}${endpoint}`, API_OPTIONS);
+      const data = await response.json();
+      console.log("data")
+      console.log(data)
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return null;
+    }
+  }
+  // Fetch initial data
+  useEffect(() => {
+    fetchData('/movie/popular').then(data => {
+      console.log('Popular Movies:', data);
+    });
+  }, []);
+
   return (
     <main>
       {/* pattern */}
@@ -13,7 +47,7 @@ const App = () => {
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
         </header>
         {/* search */}
-        <Search />
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
 
-function Search() {
+function Search({ searchTerm, setSearchTerm }) {
     return (
         <div className="search">
             <div>
                 <img src="./searchIcon.png" alt="Search Icon" />
-                <input type="text" placeholder="Search Movies..." />
+                <input type="text" placeholder="Search through thousands of movies..."
+                    value={searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)} />
             </div>
         </div>
     )
